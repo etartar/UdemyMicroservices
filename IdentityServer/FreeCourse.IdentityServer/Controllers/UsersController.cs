5208a177a05c8446f3543700a4bf7fbbf535d96c
Emir TARTAR <emirtartar35@gmail.com>
2022-04-20 22:04:09 +0300

    Identity Server UsersController Authorize

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UsersController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UsersController.cs
@@ -1,14 +1,17 @@
 ﻿using FreeCourse.IdentityServer.Dtos;
 using FreeCourse.IdentityServer.Models;
 using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace FreeCourse.IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
--- IdentityServer/FreeCourse.IdentityServer/Startup.cs
+++ IdentityServer/FreeCourse.IdentityServer/Startup.cs
@@ -28,6 +28,8 @@ namespace FreeCourse.IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalApiAuthentication();
+
             services.AddControllersWithViews();
 
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -81,6 +83,7 @@ namespace FreeCourse.IdentityServer
 
             app.UseRouting();
             app.UseIdentityServer();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
