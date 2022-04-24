d48d9a23525d630b57d6756166102d6bf3c30a0a
Emir TARTAR <emirtartar35@gmail.com>
2022-04-24 12:49:20 +0300

    Protect PhotoStock.API(microservice)

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Startup.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Startup.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,19 @@ namespace FreeCourse.Services.PhotoStock
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.PhotoStock", Version = "v1" });
             });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = Configuration["IdentityServerURL"];
+                    options.Audience = "resource_photo_stock";
+                    options.RequireHttpsMetadata = false;
+                });
+
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +62,8 @@ namespace FreeCourse.Services.PhotoStock
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
