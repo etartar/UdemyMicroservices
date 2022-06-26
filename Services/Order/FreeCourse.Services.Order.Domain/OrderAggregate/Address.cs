592b88cc85a4663e06107feb0ff43e230c698bc2
Emir TARTAR <emirtartar35@gmail.com>
2022-06-26 15:47:38 +0300

    Order/OrderItem/Address sınıflarının oluşturulması

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address : ValueObject
+    {
+        public Address(string province, string district, string street, string zipCode, string line)
+        {
+            Province = province;
+            District = district;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+
+        public string Province { get; private set; }
+        public string District { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+
+        public void SetZipCode(string zipCode)
+        {
+            // Business code -> business kuralı var ise burada tanımlıyorsun.
+
+            ZipCode = zipCode;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;
+            yield return District;
+            yield return Street;
+            yield return ZipCode;
+            yield return Line;
+        }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public decimal Price { get; private set; }
+
+        public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+    }
+}
