1bb870de3076c62be1f44cbb5e04bde6f92264d6
Emir TARTAR <emirtartar35@gmail.com>
2022-06-26 20:10:01 +0300

    AutoMapper eklenmesi

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
