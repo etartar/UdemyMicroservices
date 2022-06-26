6b6cbb8de65cc4752fe75eb4362f0ca9d677b1bf
Emir TARTAR <emirtartar35@gmail.com>
2022-06-26 19:14:48 +0300

    OrderDbContext'in oluşturulması

---
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FreeCourse.Services.Order.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "Order";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.OrderAggregate.Order> Orders { get; set; }
+        public DbSet<Domain.OrderAggregate.OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().OwnsOne(o => o.Address).WithOwner();
+
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().ToTable("OrderItems", DEFAULT_SCHEMA);
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
