57bbc2fea150fe18d7343803e0ed43f131954afa
Emir TARTAR <emirtartar35@gmail.com>
2022-09-17 11:52:01 +0300

    IdentityService / GetAccessTokenByRefreshToken

--- Frontends/FreeCourse.Web/Services/IdentityService.cs
+++ Frontends/FreeCourse.Web/Services/IdentityService.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -34,20 +35,7 @@ namespace FreeCourse.Web.Services
 
         public async Task<Response<bool>> SignIn(SignInInput signInInput)
         {
-            #region Discovery
-
-            var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
-            {
-                Address = _serviceApiSettings.BaseUri,
-                Policy = new DiscoveryPolicy { RequireHttps = false }
-            });
-
-            if (disco.IsError)
-            {
-                throw disco.Exception;
-            }
-
-            #endregion
+            DiscoveryDocumentResponse disco = await GetDiscovery();
 
             #region Get Token
 
@@ -95,24 +83,7 @@ namespace FreeCourse.Web.Services
             ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
 
             var authenticationProperties = new AuthenticationProperties();
-            authenticationProperties.StoreTokens(new List<AuthenticationToken>
-            {
-                new AuthenticationToken
-                {
-                    Name = OpenIdConnectParameterNames.AccessToken,
-                    Value = token.AccessToken
-                },
-                new AuthenticationToken
-                {
-                    Name = OpenIdConnectParameterNames.RefreshToken,
-                    Value = token.RefreshToken
-                },
-                new AuthenticationToken
-                {
-                    Name = OpenIdConnectParameterNames.ExpiresIn,
-                    Value = DateTime.Now.AddSeconds(token.ExpiresIn).ToString("o", CultureInfo.InvariantCulture)
-                }
-            });
+            authenticationProperties.StoreTokens(GetAuthenticationTokens(token.AccessToken, token.RefreshToken, token.ExpiresIn));
             authenticationProperties.IsPersistent = signInInput.IsRemember;
 
             #endregion
@@ -124,12 +95,80 @@ namespace FreeCourse.Web.Services
 
         public async Task<TokenResponse> GetAccessTokenByRefreshToken()
         {
-            throw new System.NotImplementedException();
+            DiscoveryDocumentResponse disco = await GetDiscovery();
+
+            var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest
+            {
+                ClientId = _clientSettings.WebClientForUser.ClientId,
+                ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+                Address = disco.TokenEndpoint,
+                RefreshToken = refreshToken,
+            };
+
+            var token = await _httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+
+            if (token.IsError)
+            {
+                return null;
+            }
+
+            var authenticationTokens = GetAuthenticationTokens(token.AccessToken, token.RefreshToken, token.ExpiresIn);
+            var authenticationResult = await _httpContextAccessor.HttpContext.AuthenticateAsync();
+            var properties = authenticationResult.Properties;
+            properties.StoreTokens(authenticationTokens);
+
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, authenticationResult.Principal, properties);
+
+            return token;
         }
 
         public async Task RevokeRefreshToken()
         {
             throw new System.NotImplementedException();
         }
+
+        #region Private Methods
+
+        private async Task<DiscoveryDocumentResponse> GetDiscovery()
+        {
+            var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceApiSettings.BaseUri,
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            });
+
+            if (disco.IsError)
+            {
+                throw disco.Exception;
+            }
+
+            return disco;
+        }
+
+        private List<AuthenticationToken> GetAuthenticationTokens(string accessToken, string refreshToken, int expiresIn)
+        {
+            return new List<AuthenticationToken>
+            {
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.AccessToken,
+                    Value = accessToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.RefreshToken,
+                    Value = refreshToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.ExpiresIn,
+                    Value = DateTime.Now.AddSeconds(expiresIn).ToString("o", CultureInfo.InvariantCulture)
+                }
+            };
+        }
+
+        #endregion
     }
 }
