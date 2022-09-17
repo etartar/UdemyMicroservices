fac1614831831bea59910079d3889d7dfb9790db
Emir TARTAR <emirtartar35@gmail.com>
2022-09-17 09:52:03 +0300

    Add IIdentityService and IdentityModel Library

---
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SignInInput
+    {
+        [Display(Name ="Email adresiniz")]
+        public string Email { get; set; }
+
+        [Display(Name ="Şifreniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using IdentityModel.Client;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SignInInput signInInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
