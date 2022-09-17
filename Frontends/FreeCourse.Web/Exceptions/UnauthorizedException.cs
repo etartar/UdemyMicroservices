47139c63c48ff1ddc228595bea140f89f5f64bdb
Emir TARTAR <emirtartar35@gmail.com>
2022-09-17 13:15:35 +0300

    TokenDelegateHandler

---
+++ Frontends/FreeCourse.Web/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FreeCourse.Web.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException() : base()
+        {
+        }
+
+        public UnauthorizedException(string message) : base(message)
+        {
+        }
+
+        public UnauthorizedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Web.Handler;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services;
 using FreeCourse.Web.Services.Interfaces;
@@ -33,7 +34,8 @@ namespace FreeCourse.Web
             services.AddHttpClient<IUserService, UserService>(opt =>
             {
                 opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
-            });
+            })
+                .AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opt =>
