d29d2a9438f2aee608b49bd095237d1280ff1522
Emir TARTAR <emirtartar35@gmail.com>
2022-09-22 20:44:45 +0300

    Basket microservice için viewmodel oluşturma

---
+++ Frontends/FreeCourse.Web/Models/Baskets/BasketItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace FreeCourse.Web.Models.Baskets
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+        private decimal? DiscountAppliedPrice { get; set; }
+
+        public void AppliedDiscount(decimal discountPrice)
+        {
+            DiscountAppliedPrice = discountPrice;
+        }
+
+        public decimal GetCurrentPrice
+        {
+            get
+            {
+                return DiscountAppliedPrice != null ? DiscountAppliedPrice.Value : Price;
+            }
+        }
+    }
+}
